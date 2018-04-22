219fc1bf99001156af105b5c2ed04d45e7c1e348
Roy Bhattacharya <anroybhattacharya@w17431.wireless.davidson.edu>
2018-04-21 20:56:07 -0400

    Add code for adding times to datset

--- aggregate.py
+++ aggregate.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pandas as pd
-import ast
-import os
-import glob, os
-import json
+import datetime as dt
 from sklearn.feature_extraction.text import CountVectorizer as CV
 from sklearn.feature_extraction.text import TfidfTransformer as TF
+
 def parse_csv():
     df_2016 = pd.read_csv("2016.csv")
     df_2017 = pd.read_csv("2017.csv")
@@ -13,12 +11,21 @@ def parse_csv():
     return df
 
 def vectorize_text(x_train, x_test):
-
    vectorizer = TF(strip_accents='unicode', analyzer = 'word')
    text_train = vectorizer.fit_transform(x_train).toarray()
    text_test = vectorizer.transform(x_test).toarray()
    text_train,text_test = pd.DataFrame(text_train),pd.DataFrame(text_test)
    return text_train, text_test
+
+"""
+Function that adds day, hour, and minute columns to the df.
+"""
+def add_time_columns(row):
+    utc_time = row['created_utc']
+    day = dt.datetime.fromtimestamp(int(utc_time)).strftime('%A')
+    hour = dt.datetime.fromtimestamp(int(utc_time)).strftime('%-H')
+    minute = dt.datetime.fromtimestamp(int(utc_time)).strftime('%-M')
+    return pd.Series([day, hour, minute], index = ['day', 'hour', 'minute'])
 
 
 def main():
@@ -34,6 +41,7 @@ def main():
     print (text_train)
 
 
-
 if __name__ == '__main__':
-    main()
+    df = parse_csv()
+    df = df.apply(lambda row: add_time_columns(row), axis=1)
+    print(df)
