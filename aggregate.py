ece6a91b4a24263a7d7bc7c04b031e0f9de595fa
Mutian <mutian@mutians-mbp-2.wireless.davidson.edu>
2018-04-24 14:53:56 -0400

    Add the random forest model

--- aggregate.py
+++ aggregate.py
@@ -4,7 +4,8 @@ import datetime as dt
 from sklearn.feature_extraction.text import CountVectorizer as CV
 from sklearn.feature_extraction.text import TfidfTransformer as TF
 from sklearn.feature_extraction.text import TfidfVectorizer as TV
-
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
 def parse_csv():
     df_2016 = pd.read_csv("2016.csv")
     df_2017 = pd.read_csv("2017.csv")
@@ -13,14 +14,14 @@ def parse_csv():
 
 def vectorize_text(x_train, x_test):
 
-    count_vect = CV(strip_accents='unicode', analyzer = 'word')
+    count_vect = CV(strip_accents='unicode', analyzer = 'word', max_features = 100000)
     X_train_counts = count_vect.fit_transform(x_train["title"])
     tfidf_transformer = TF()
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
     X_new_counts = count_vect.transform(x_test['title'])
     X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-    return X_train_tfidf, X_new_tfidf,count_vect
+    return X_train_tfidf, X_new_tfidf,count_vect,tfidf_transformer
 
 
 """
@@ -32,22 +33,29 @@ def add_time_columns(row):
     hour = dt.datetime.fromtimestamp(int(utc_time)).strftime('%-H')
     minute = dt.datetime.fromtimestamp(int(utc_time)).strftime('%-M')
     return pd.Series([day, hour, minute], index = ['day', 'hour', 'minute'])
+def random_forest(X_train,y_train, X_test, y_test):
+    regr = RandomForestRegressor(max_depth=2, random_state=0)
+    regr.fit(X_train, y_train)
+    predicted = regr.predict(X_test)
+    return r2_score(y_test, predicted)
 
 
 def main():
-    df = parse_csv()
-    df = df.sample(frac=1)
-    df = df.reset_index(drop = True)
+    # df = parse_csv()
+    # df = df.dropna(how = 'any')
+    # df = df.sample(frac=1)
+    # df = df.reset_index(drop = True)
+    # df.to_pickle('aggregate.pkl')
+    df = pd.read_pickle('aggregate.pkl')
+    print df.shape
     train_set = df.iloc[0:int(df.shape[0]*0.8)]
     test_set = df.iloc[int(df.shape[0]*0.8):df.shape[0]-1]
     x_train = train_set.drop(columns = ['score','created_utc'])
     x_test = test_set.drop(columns = ['score','created_utc'])
-
-    text_train, text_test,count_vect = vectorize_text(x_train,x_test)
-    # print (text_train)
+    text_train, text_test,count_vect,tfidf_transformer = vectorize_text(x_train,x_test)
+    print text_train.shape
+    print random_forest(text_train,train_set['score'],text_test,test_set['score'])
 
 
 if __name__ == '__main__':
-    df = parse_csv()
-    df = df.apply(lambda row: add_time_columns(row), axis=1)
-    print(df)
+    main()
